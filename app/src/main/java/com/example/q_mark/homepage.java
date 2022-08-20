75d352653e0525c23f5b512ca3f1d7a9e571d49d
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2022-08-20 22:30:43 +0600

    side_menu add

--- app/src/main/java/com/example/q_mark/homepage.java
+++ app/src/main/java/com/example/q_mark/homepage.java
@@ -1,14 +1,33 @@
 package com.example.q_mark;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class homepage extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       getSupportActionBar().hide();
         setContentView(R.layout.activity_homepage);
     }
+
+//    @Override
+//    public void onBackPressed() {
+//        if(drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        }
+//        else {
+//            super.onBackPressed();
+//        }
+//
+//    }
+
 }
