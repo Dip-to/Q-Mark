993c2ed081c158a1cf86c103c28ec6e7a3ad169a
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2022-08-21 01:20:10 +0600

    side_menu add

--- app/src/main/java/com/example/q_mark/homepage.java
+++ app/src/main/java/com/example/q_mark/homepage.java
@@ -1,5 +1,6 @@
 package com.example.q_mark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,8 +9,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class homepage extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -17,6 +21,59 @@ public class homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_homepage);
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Chat()).commit();
+            navigationView.setCheckedItem(R.id.side_menu_chat);
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Profile()).commit();
+            navigationView.setCheckedItem(R.id.side_menu_profile);
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Notification()).commit();
+            navigationView.setCheckedItem(R.id.side_menu_notification);
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Friends()).commit();
+            navigationView.setCheckedItem(R.id.side_menu_friends);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Addfriend()).commit();
+            navigationView.setCheckedItem(R.id.side_menu_addfriend);
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Upload()).commit();
+            navigationView.setCheckedItem(R.id.side_menu_upload);
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Settings()).commit();
+            navigationView.setCheckedItem(R.id.side_menu_settings);
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Course()).commit();
+            navigationView.setCheckedItem(R.id.side_menu_course);
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Home()).commit();
+            navigationView.setCheckedItem(R.id.side_menu_home);
+
+
+        }
+
+
     }
 
     @Override
@@ -29,4 +86,55 @@ public class homepage extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.side_menu_chat:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Chat()).commit();
+                break;
+
+            case R.id.side_menu_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Home()).commit();
+                break;
+
+            case R.id.side_menu_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Profile()).commit();
+                break;
+            case R.id.side_menu_friends:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Friends()).commit();
+                break;
+
+            case R.id.side_menu_notification:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Notification()).commit();
+                break;
+
+            case R.id.side_menu_course:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Course()).commit();
+                break;
+
+            case R.id.side_menu_addfriend:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Addfriend()).commit();
+                break;
+
+            case R.id.side_menu_upload:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Upload()).commit();
+                break;
+
+            case R.id.side_menu_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Settings()).commit();
+
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
