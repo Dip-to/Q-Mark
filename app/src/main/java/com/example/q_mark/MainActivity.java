c45e4cdde71ae5da127d34dcc986fb7adacd9131
Dip-to <2019-617-833@student.cse.du.ac.bd>
2022-08-21 02:55:42 +0600

    welcome page

--- app/src/main/java/com/example/q_mark/MainActivity.java
+++ app/src/main/java/com/example/q_mark/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_login);
-        startActivity(new Intent(MainActivity.this, Login.class));
+        startActivity(new Intent(MainActivity.this, Welcome_page.class));
     }
 }
---
+++ app/src/main/java/com/example/q_mark/Welcome_page.java
@@ -0,0 +1,47 @@
+package com.example.q_mark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Welcome_page extends AppCompatActivity {
+    private TextView t1,t2,t3;
+    private ImageView im1,im2;
+    Animation top,bottom;
+    private  static  int SPLASH_SCREEN =2500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_welcom_page);
+        t1=findViewById(R.id.textView);
+        t2=findViewById(R.id.to);
+        t3=findViewById(R.id.name);
+        im1=findViewById(R.id.imageView);
+        im2=findViewById(R.id.imageView2);
+        top = AnimationUtils.loadAnimation(this, R.anim.top);
+        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom);
+
+        im1.setAnimation(top);
+        im2.setAnimation(bottom);
+        t1.setAnimation(bottom);
+        t2.setAnimation(top);
+        t3.setAnimation(bottom);
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Welcome_page.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+    }
+}
