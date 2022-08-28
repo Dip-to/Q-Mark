fa452d2a4da61d679abdcb5ca3201761e62acc49
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2022-08-28 22:49:29 +0600

    bottom navigatiion bar

--- app/src/main/java/com/example/q_mark/homepage.java
+++ app/src/main/java/com/example/q_mark/homepage.java
@@ -1,20 +1,22 @@
 package com.example.q_mark;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class homepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +25,23 @@ public class homepage extends AppCompatActivity implements NavigationView.OnNavi
         setContentView(R.layout.activity_homepage);
 
 
+        BottomNavigationView bottom_nav=findViewById(R.id.bottom_navigation);
+        bottom_nav.setOnItemSelectedListener(nav_listener);
+
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         drawer = findViewById(R.id.drawer_layout);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open,
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar,R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close);
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setCheckedItem(R.id.side_menu_home);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -74,6 +83,7 @@ public class homepage extends AppCompatActivity implements NavigationView.OnNavi
         }
 
 
+
     }
 
     @Override
@@ -85,6 +95,9 @@ public class homepage extends AppCompatActivity implements NavigationView.OnNavi
         }
 
     }
+
+
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -137,4 +150,35 @@ public class homepage extends AppCompatActivity implements NavigationView.OnNavi
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    private BottomNavigationView.OnItemSelectedListener nav_listener=new BottomNavigationView.OnItemSelectedListener(){
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=new Home();
+            switch (item.getItemId()){
+                case R.id.bottom_home:
+                     selectedFragment=new Home();
+                    break;
+                case R.id.bottom_profile:
+                    selectedFragment=new Profile();
+                    break;
+                case R.id.bottom_search:
+                    selectedFragment=new Search();
+                    break;
+                case R.id.bottom_notification:
+                    selectedFragment=new Notification();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+            return true;
+
+        }
+    };
+
+
+
+
 }
