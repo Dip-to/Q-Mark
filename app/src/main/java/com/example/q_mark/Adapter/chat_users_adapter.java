e07976dba0879837f456894f6a14c36abcb68f90
Dip-to <2019-617-833@student.cse.du.ac.bd>
2022-10-03 22:38:15 +0600

    Chat Conversation page

--- app/src/main/java/com/example/q_mark/Adapter/chat_users_adapter.java
+++ app/src/main/java/com/example/q_mark/Adapter/chat_users_adapter.java
@@ -1,9 +1,11 @@
 package com.example.q_mark.Adapter;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -11,35 +13,49 @@ import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.q_mark.Model.User;
+import com.example.q_mark.R;
 import com.example.q_mark.databinding.ItemContainerChatUserBinding;
+import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
 public class chat_users_adapter extends RecyclerView.Adapter<chat_users_adapter.UserViewHolder> {
 
-    private final List<User> users;
+    Context context;
+    ArrayList<User> users;
 
-    public  chat_users_adapter(List<User> users){
-        this.users=users;
+    public chat_users_adapter(Context context, ArrayList<User> users) {
+        this.context = context;
+        this.users = users;
+
     }
+
 
     @NonNull
     @Override
     public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        ItemContainerChatUserBinding itemContainerChatUserBinding = ItemContainerChatUserBinding.inflate(
-                LayoutInflater.from(parent.getContext()),
-                parent,
-                false
-        );
-
-        return new UserViewHolder(itemContainerChatUserBinding);
+//        ItemContainerChatUserBinding itemContainerChatUserBinding = ItemContainerChatUserBinding.inflate(
+//                LayoutInflater.from(parent.getContext()),
+//                parent,
+//                false
+//        );
+//
+//        return new UserViewHolder(itemContainerChatUserBinding);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_container_chat_user,parent,false);
+        return new UserViewHolder(view);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
-        holder.setUsersData(users.get(position));
+        //holder.setUsersData(users.get(position));
+        User chatUser=users.get(position);
+        holder.binding.chatTextName.setText(chatUser.getName());
+        System.out.println(chatUser.getName()+"adapt");
+        holder.binding.textEmail.setText(chatUser.getEmail());
+        Picasso.with(context).load(chatUser.getPimage()).placeholder(R.drawable.ic_profile).into(holder.binding.chatImageprofile);
+
 
     }
 
@@ -52,26 +68,12 @@ public class chat_users_adapter extends RecyclerView.Adapter<chat_users_adapter.
 
 
         ItemContainerChatUserBinding binding;
-
-        UserViewHolder(ItemContainerChatUserBinding itemContainerChatUserBinding){
-            super(itemContainerChatUserBinding.getRoot());
-            binding = itemContainerChatUserBinding;
+        UserViewHolder(View itemContainerChatUserBinding){
+            super(itemContainerChatUserBinding);
+            binding = ItemContainerChatUserBinding.bind(itemContainerChatUserBinding);
         }
 
-        @RequiresApi(api = Build.VERSION_CODES.O)
-        void setUsersData(User chatUser){
-            binding.chatTextName.setText(chatUser.getName());
-            binding.textEmail.setText(chatUser.getEmail());
-            binding.chatImageprofile.setImageBitmap(getUserImage(chatUser.getPimage()));
-        }
     }
 
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private Bitmap getUserImage(String encodedImage){
-        byte[] bytes = Base64.getDecoder().decode(encodedImage);
-        return BitmapFactory.decodeByteArray(bytes,0,bytes.length);
-
-    }
 
 }
--- app/src/main/java/com/example/q_mark/Fragments/Chat.java
+++ app/src/main/java/com/example/q_mark/Fragments/Chat.java
@@ -1,6 +1,8 @@
 package com.example.q_mark.Fragments;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +11,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.q_mark.Adapter.chat_users_adapter;
+import com.example.q_mark.Model.User;
 import com.example.q_mark.databinding.ChatBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 public class Chat  extends Fragment {
     private ChatBinding chatBinding;
+
+    ArrayList<User> list=new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,6 +36,69 @@ public class Chat  extends Fragment {
         chatBinding= ChatBinding.inflate(inflater,container,false);
         LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
         chatBinding.chatUserRecycleView.setLayoutManager(linearLayoutManager);
+
+        chat_users_adapter chat_users_adapter=new chat_users_adapter(getContext(),list);
+        chatBinding.chatUserRecycleView.setAdapter(chat_users_adapter);
+
+
+
+
+        FirebaseDatabase.getInstance().getReference().child("User").child(FirebaseAuth.getInstance().getUid())
+                .child("following") .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        list.clear();
+                        for(DataSnapshot dataSnapshot: snapshot.getChildren())
+                        {
+                            FirebaseDatabase.getInstance().getReference().child("User").child(dataSnapshot.getKey()).addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    User user=snapshot.getValue(User.class);
+                                    user.setUid(dataSnapshot.getKey());
+                                    System.out.println(dataSnapshot.getKey());
+                                    System.out.println(user.getName());
+                                    list.add(user);
+                                    chat_users_adapter.notifyDataSetChanged();
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+
+                        }
+
+
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+//        firebaseDatabase.getReference().child("User").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                list.clear();
+//                for(DataSnapshot dataSnapshot: snapshot.getChildren())
+//                {
+//
+//                    User user=dataSnapshot.getValue(User.class);
+//                    user.setUid(dataSnapshot.getKey());
+//                    System.out.println(dataSnapshot.getKey());
+//                    if(!dataSnapshot.getKey().equals(FirebaseAuth.getInstance().getUid()))
+//                    {
+//                        list.add(user);
+//                    }
+//                }
+//                us.notifyDataSetChanged();
+//            }
         return chatBinding.getRoot();
 
     }
