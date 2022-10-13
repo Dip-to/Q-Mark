cf27382bd740b64dc435e5c826b6eb8228dcea6a
adil-olin <olinafseradil@gmail.com>
2022-10-14 00:30:28 +0600

    Upload page updated slightly

--- app/src/main/java/com/example/q_mark/Fragments/Upload.java
+++ app/src/main/java/com/example/q_mark/Fragments/Upload.java
@@ -1,15 +1,21 @@
 package com.example.q_mark.Fragments;
 
+import java.io.*;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,6 +41,7 @@ public class Upload extends Fragment {
 
     private Button UploadPageChooseFileButton,UploadPageUploadButton;
     private TextView UploadPageChooseFileTextView;
+    private ImageView imageView;
 
     //uri to store file
     private Uri filePath;
@@ -57,6 +64,7 @@ public class Upload extends Fragment {
         UploadPageChooseFileButton = (Button) getView().findViewById(R.id.choose_btn);
         UploadPageUploadButton = (Button) getView().findViewById(R.id.upload_btn);
         UploadPageChooseFileTextView = (TextView) getView().findViewById(R.id.upload_txtfield);
+        imageView = (ImageView) getView().findViewById(R.id.uploadedImage);
 
         UploadPageChooseFileButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,5 +79,46 @@ public class Upload extends Fragment {
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent,"Select an Image"),PICK_IMAGE_REQUEST);
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == -1 && data!=null && data.getData() != null)
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+                UploadPageChooseFileTextView.setText(getFileName(filePath));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
